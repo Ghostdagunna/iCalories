86298eccdc90e862a5b2b3b63f35f976609d931a
Jordan Mck <ghostdagunna@gmail.com>
2023-10-09 12:09:20 -0500

    Initial Commit

---
+++ iCalories/iCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCaloriesApp.swift
+//  iCalories
+//
+//  Created by Jordan Mck on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct iCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
